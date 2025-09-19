50c4e86806e21f346a7513867cdd24d4998c21db
Adedayoke <adedayoke2006@gmail.com>
2025-09-19 16:42:27 +0100

    Task 1

--- alx-project-0x01/pages/posts/index.tsx
+++ alx-project-0x01/pages/posts/index.tsx
@@ -1,7 +1,11 @@
+import Header from '@/components/layout/Header'
 import React from 'react'
 
 export default function index() {
   return (
-    <div>index</div>
+    <div>
+        <Header />
+        
+    </div>
   )
 }
--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -1,7 +1,10 @@
+import Header from '@/components/layout/Header'
 import React from 'react'
 
 export default function index() {
   return (
-    <div>index</div>
+    <div>
+        <Header />
+    </div>
   )
 }
